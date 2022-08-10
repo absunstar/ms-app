49cb68a012c7328ce68075d654fbb66ecc897d41
AbduAllah Gabbar <you@example.com>
2022-08-10 04:40:51 +0200

    update

---
+++ apps_employment/company/app.js
@@ -0,0 +1,266 @@
+module.exports = function init(site) {
+  const $company = site.connectCollection('Company');
+
+  site.get({
+    name: 'images',
+    path: __dirname + '/site_files/images/',
+  });
+
+  site.get({
+    name: 'Company',
+    path: __dirname + '/site_files/html/index.html',
+    parser: 'html',
+    compress: true,
+  });
+
+  site.on('[company][created]', (doc) => {
+    $company.add(
+      {
+        code: '1-Test',
+        name_ar: 'شركة إفتراضية',
+        name_en: 'Default Company',
+        image_url: '/images/company.png',
+        active: true,
+      },
+      (err, doc1) => {}
+    );
+  });
+
+  site.post('/api/company/add', (req, res) => {
+    let response = {
+      done: false,
+    };
+
+    if (!req.session.user) {
+      response.error = 'Please Login First';
+      res.json(response);
+      return;
+    }
+
+    let company_doc = req.body;
+    company_doc.$req = req;
+    company_doc.$res = res;
+
+    company_doc.add_user_info = site.security.getUserFinger({
+      $req: req,
+      $res: res,
+    });
+
+    if (typeof company_doc.active === 'undefined') {
+      company_doc.active = true;
+    }
+
+    $company.findOne(
+      {
+        where: {
+          $or: [
+            {
+              name_ar: company_doc.name_ar,
+            },
+            {
+              name_en: company_doc.name_en,
+            },
+          ],
+        },
+      },
+      (err, doc) => {
+        if (!err && doc) {
+          response.error = 'Name Exists';
+          res.json(response);
+        } else {
+          // let d = new Date();
+          // d.setFullYear(d.getFullYear() + 1);
+          // d.setMonth(1);
+          let num_obj = {
+            screen: 'company',
+            date: new Date(),
+          };
+
+          // let cb = site.getNumbering(num_obj);
+          // if (!company_doc.code && !cb.auto) {
+          //   response.error = 'Must Enter Code';
+          //   res.json(response);
+          //   return;
+          // } else if (cb.auto) {
+          //   company_doc.code = cb.code;
+          // }
+
+          $company.add(company_doc, (err, doc) => {
+            if (!err) {
+              response.done = true;
+              response.doc = doc;
+            } else {
+              response.error = err.message;
+            }
+            res.json(response);
+          });
+        }
+      }
+    );
+  });
+
+  site.post('/api/company/update', (req, res) => {
+    let response = {
+      done: false,
+    };
+
+    if (!req.session.user) {
+      response.error = 'Please Login First';
+      res.json(response);
+      return;
+    }
+
+    let company_doc = req.body;
+
+    company_doc.edit_user_info = site.security.getUserFinger({
+      $req: req,
+      $res: res,
+    });
+
+    if (company_doc.id) {
+      $company.findOne(
+        {
+          where: {
+            $or: [
+              {
+                name_ar: company_doc.name_ar,
+              },
+              {
+                name_en: company_doc.name_en,
+              },
+            ],
+          },
+        },
+        (err, doc) => {
+          if (!err && doc && doc.id != company_doc.id) {
+            response.error = 'Name Exists';
+            res.json(response);
+          } else {
+            $company.edit(
+              {
+                where: {
+                  id: company_doc.id,
+                },
+                set: company_doc,
+                $req: req,
+                $res: res,
+              },
+              (err) => {
+                if (!err) {
+                  response.done = true;
+                } else {
+                  response.error = 'Code Already Exist';
+                }
+                res.json(response);
+              }
+            );
+          }
+        }
+      );
+    } else {
+      response.error = 'no id';
+      res.json(response);
+    }
+  });
+
+  site.post('/api/company/view', (req, res) => {
+    let response = {
+      done: false,
+    };
+
+    if (!req.session.user) {
+      response.error = 'Please Login First';
+      res.json(response);
+      return;
+    }
+
+    $company.findOne(
+      {
+        where: {
+          id: req.body.id,
+        },
+      },
+      (err, doc) => {
+        if (!err) {
+          response.done = true;
+          response.doc = doc;
+        } else {
+          response.error = err.message;
+        }
+        res.json(response);
+      }
+    );
+  });
+
+  site.post('/api/company/delete', (req, res) => {
+    let response = {
+      done: false,
+    };
+
+    if (!req.session.user) {
+      response.error = 'Please Login First';
+      res.json(response);
+      return;
+    }
+
+    let id = req.body.id;
+
+    if (id) {
+      $company.delete(
+        {
+          id: id,
+          $req: req,
+          $res: res,
+        },
+        (err, result) => {
+          if (!err) {
+            response.done = true;
+          } else {
+            response.error = err.message;
+          }
+          res.json(response);
+        }
+      );
+    } else {
+      response.error = 'no id';
+      res.json(response);
+    }
+  });
+
+  site.post('/api/company/all', (req, res) => {
+    let response = {
+      done: false,
+    };
+
+    let where = req.body.where || {};
+  
+    if (where['name_ar']) {
+      where['name_ar'] = site.get_RegExp(where['name_ar'], 'i');
+    }
+
+    if (where['name_en']) {
+      where['name_en'] = site.get_RegExp(where['name_en'], 'i');
+    }
+
+    $company.findMany(
+      {
+        select: req.body.select || {},
+        where: where,
+        sort: req.body.sort || {
+          id: -1,
+        },
+        limit: req.body.limit,
+      },
+      (err, docs, count) => {
+        if (!err) {
+          response.done = true;
+          response.list = docs;
+          response.count = count;
+        } else {
+          response.error = err.message;
+        }
+        res.json(response);
+      }
+    );
+  });
+};
---
+++ apps_employment/company/site_files/js/index.js
@@ -0,0 +1,239 @@
+app.controller('company', function ($scope, $http, $timeout) {
+  $scope._search = {};
+
+  $scope.company = {};
+
+  $scope.displayAddCompany = function () {
+    $scope.error = '';
+    $scope.company = {
+      image_url: '/images/company.png',
+      active: true,
+    };
+
+    site.showModal('#companyAddModal');
+  };
+
+  $scope.addCompany = function () {
+    $scope.error = '';
+    const v = site.validated('#companyAddModal');
+    if (!v.ok) {
+      $scope.error = v.messages[0].ar;
+      return;
+    }
+
+    $scope.busy = true;
+    $http({
+      method: 'POST',
+      url: '/api/company/add',
+      data: $scope.company,
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          site.hideModal('#companyAddModal');
+          $scope.getCompanyList();
+        } else {
+          $scope.error = response.data.error;
+          if (response.data.error.like('*Must Enter Code*')) {
+            $scope.error = '##word.must_enter_code##';
+          } else if (response.data.error.like('*Name Exists*')) {
+            $scope.error = '##word.name_already_exists##';
+          }
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+  };
+
+  $scope.displayUpdateCompany = function (company) {
+    $scope.error = '';
+    $scope.viewCompany(company);
+    $scope.company = {};
+    site.showModal('#companyUpdateModal');
+  };
+
+  $scope.updateCompany = function (company) {
+    $scope.error = '';
+    const v = site.validated('#companyUpdateModal');
+    if (!v.ok) {
+      $scope.error = v.messages[0].ar;
+      return;
+    }
+    $scope.busy = true;
+    $http({
+      method: 'POST',
+      url: '/api/company/update',
+      data: company,
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          site.hideModal('#companyUpdateModal');
+          $scope.getCompanyList();
+        } else {
+          $scope.error = response.data.error;
+          if (response.data.error.like('*Name Exists*')) {
+            $scope.error = '##word.name_already_exists##';
+          }
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+  };
+
+   $scope.updateActivate = function (company) {
+    $scope.error = '';
+
+    $scope.busy = true;
+    $http({
+      method: 'POST',
+      url: '/api/company/update',
+      data: company,
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+        } else {
+          $scope.error = response.data.error;
+          if (response.data.error.like('*Name Exists*')) {
+            $scope.error = '##word.name_already_exists##';
+          }
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+  };
+ 
+  $scope.displayDetailsCompany = function (company) {
+    $scope.error = '';
+    $scope.viewCompany(company);
+    $scope.company = {};
+    site.showModal('#companyViewModal');
+  };
+
+  $scope.viewCompany = function (company) {
+    $scope.busy = true;
+    $scope.error = '';
+    $http({
+      method: 'POST',
+      url: '/api/company/view',
+      data: {
+        id: company.id,
+      },
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          $scope.company = response.data.doc;
+        } else {
+          $scope.error = response.data.error;
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+  };
+
+  $scope.displayDeleteCompany = function (company) {
+    $scope.error = '';
+    $scope.viewCompany(company);
+    $scope.company = {};
+    site.showModal('#companyDeleteModal');
+  };
+
+  $scope.deleteCompany = function () {
+    $scope.busy = true;
+    $scope.error = '';
+
+    $http({
+      method: 'POST',
+      url: '/api/company/delete',
+      data: {
+        id: $scope.company.id,
+      },
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          site.hideModal('#companyDeleteModal');
+          $scope.getCompanyList();
+        } else {
+          $scope.error = response.data.error;
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+  };
+
+  $scope.searchAll = function () {
+    $scope.getCompanyList($scope.search);
+    site.hideModal('#companySearchModal');
+    $scope.search = {};
+  };
+
+  $scope.getCompanyList = function (where) {
+    $scope.busy = true;
+    $scope.list = [];
+    $http({
+      method: 'POST',
+      url: '/api/company/all',
+      data: {
+        where: where,
+      },
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done && response.data.list.length > 0) {
+          $scope.list = response.data.list;
+          $scope.count = response.data.count;
+          site.hideModal('#companySearchModal');
+          $scope.search = {};
+        }
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    );
+  };
+
+  $scope.getNumberingAuto = function () {
+    $scope.error = '';
+    $scope.busy = true;
+    $http({
+      method: 'POST',
+      url: '/api/numbering/get_automatic',
+      data: {
+        screen: 'company',
+      },
+    }).then(
+      function (response) {
+        $scope.busy = false;
+        if (response.data.done) {
+          $scope.disabledCode = response.data.isAuto;
+        }
+      },
+      function (err) {
+        $scope.busy = false;
+        $scope.error = err;
+      }
+    );
+  };
+
+  $scope.displaySearchModal = function () {
+    $scope.error = '';
+    site.showModal('#companySearchModal');
+  };
+
+  $scope.getCompanyList();
+  $scope.getNumberingAuto();
+});
