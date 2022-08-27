bd5786378e372ad4aa3ac88dff535abc1ce250bf
absunstar <absunstar@gmail.com>
2022-08-27 19:34:37 +0200

    update

--- apps_employment/jobs/app.js
+++ apps_employment/jobs/app.js
@@ -110,11 +110,6 @@ module.exports = function init(site) {
       done: false,
     };
 
-    if (!req.session.user) {
-      response.error = 'Please Login First';
-      res.json(response);
-      return;
-    }
 
     $jobs.findOne(
       {
