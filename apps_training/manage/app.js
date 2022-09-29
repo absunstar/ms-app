ff188e21b528e73adfd27cbd9a31d1e323f8b9b2
absunstar <absunstar@gmail.com>
2022-09-29 04:04:30 +0200

    update

--- apps_training/manage/app.js
+++ apps_training/manage/app.js
@@ -1,9 +1,37 @@
 module.exports = function init(site) {
   const $manage = site.connectCollection('Manage');
-  site.manage_doc = {};
+  site.manage_doc = {
+    title: 'Training',
+    slug: 'Update Your Skills',
+    nav_color: '#000000',
+    nav_background: '#ffffff',
+    fonts: 'Arabic, sans-serif',
+    logo: { url: '/images/logo.png' },
+    logo1: { url: '/images/logo1.png' },
+    logo2: { url: '/images/logo2.png' },
+    logo3: { url: '/images/logo3.png' },
+    trainer_number: 20,
+    training_number: 50,
+    trainee_number: 3000,
+    numbers_title: 'numbers talk about us',
+    numbers_description: '',
+    slider1: { title: '', description: '', logo: { url: '/images/banner1.jpg' } },
+    slider2: { title: '', description: '', logo: { url: '/images/banner1.jpg' } },
+    slider3: { title: '', description: '', logo: { url: '/images/banner1.jpg' } },
+    partners_logo_list: [],
+  };
+  site.setting = { ...site.setting, ...site.manage_doc };
   $manage.findOne({}, (err, doc) => {
     if (!err && doc) {
       site.manage_doc = doc;
+      site.setting = { ...site.setting, ...site.manage_doc };
+    } else {
+      $manage.add(site.manage_doc, (err, doc) => {
+        if (!err && doc) {
+          site.manage_doc = doc;
+          site.setting = { ...site.setting, ...site.manage_doc };
+        }
+      });
     }
   });
 
@@ -21,27 +49,10 @@ module.exports = function init(site) {
 
   site.post('/api/manage/get', (req, res) => {
     let response = {
-      done: false,
+      done: true,
+      doc: site.manage_doc,
     };
-
-    if (site.manage_doc && Object.keys(site.manage_doc).length != 0) {
-      response.done = true;
-      response.doc = site.manage_doc;
-      res.json(response);
-    } else {
-   
-      $manage.add({partners_logo_list : []}, (err, doc) => {
-        if (!err && doc) {
-          response.done = true;
-          response.doc = doc;
-          site.manage_doc = response.doc;
-          res.json(response);
-        } else {
-          response.error = err.message;
-          res.json(response);
-        }
-      });
-    }
+    res.json(response);
   });
 
   site.getManage = function (callback) {
@@ -50,7 +61,6 @@ module.exports = function init(site) {
       callback(site.manage_doc);
       return site.manage_doc;
     }
-
   };
 
   site.post('/api/manage/save', (req, res) => {
@@ -58,13 +68,12 @@ module.exports = function init(site) {
       done: false,
     };
 
-
-
     let data = req.data;
     $manage.update(data, (err, result) => {
       if (!err) {
         response.done = true;
         site.manage_doc = data;
+        site.setting = { ...site.setting, ...site.manage_doc };
       } else {
         response.error = err.message;
       }
