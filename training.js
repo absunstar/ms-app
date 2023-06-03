b36f4c0771153ecf4e62af4affe1c66d89b5cdaf
absunstar <absunstar@gmail.com>
2023-06-03 22:01:13 +0300

    update

--- training.js
+++ training.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const site = require('../isite')({
   port: [44442],
   lang: 'en',
-  version: '3.166.25',
+  version: '3.156.30',
   name: 'training',
   theme: 'theme_paper',
   savingTime: 1,
